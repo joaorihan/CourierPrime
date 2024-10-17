3ba7a0f156877b8883bb092307bbdfdc8d66d9c2
joaorihan <joao.rihan@gmail.com>
2024-10-17 11:22:09 -0300

    feat: AbstractCommand

---
+++ src/main/java/com/joaorihan/courierprime/command/AbstractCommand.java
@@ -0,0 +1,58 @@
+package com.joaorihan.courierprime.command;
+
+import com.joaorihan.courierprime.CourierPrime;
+import com.joaorihan.courierprime.Message;
+import lombok.Getter;
+import lombok.Setter;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandMap;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.defaults.BukkitCommand;
+
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.List;
+
+@Setter
+@Getter
+public abstract class AbstractCommand extends BukkitCommand {
+
+    private CourierPrime plugin;
+
+    public AbstractCommand(String command, String[] aliases, String description, String permission) {
+        super(command);
+
+        this.setAliases(Arrays.asList(aliases));
+        this.setDescription(description);
+        this.setPermission(permission);
+        this.setPermissionMessage(ChatColor.RED + Message.ERROR_NO_PERMS + permission);
+
+        try {
+            Field field = Bukkit.getServer().getClass().getDeclaredField("commandMap");
+            field.setAccessible(true);
+            CommandMap map = (CommandMap) field.get(Bukkit.getServer());
+            map.register(command, "courierprime", this);
+        } catch (NoSuchFieldException | IllegalAccessException e){
+            e.printStackTrace();
+        }
+
+        setPlugin(CourierPrime.getPlugin());
+    }
+
+    @Override
+    public boolean execute(CommandSender commandSender, String s, String[] strings) {
+        execute(commandSender, strings);
+        return false;
+    }
+
+    public abstract void execute(CommandSender sender, String[] args);
+
+    @Override
+    public List<String> tabComplete(CommandSender sender, String alias, String[] args) throws IllegalArgumentException {
+        return onTabComplete(sender, args);
+    }
+
+    public abstract List<String> onTabComplete(CommandSender sender, String[] args);
+
+}
